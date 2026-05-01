180c8d826c2a7115f93290db6fa32347c53841c0
Rajat Kumar Bal <rajatkumarbal961@gmail.com>
2026-05-02 03:19:07 +0530

    added cloudinary config

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os, dj_database_url
+import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR/'templates'
@@ -133,3 +134,9 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+)
